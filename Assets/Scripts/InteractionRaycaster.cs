2428dd426b8e59c6c509cebfe9ab119397e19591
guadalupebaquer-eng <c.guadalupe.baquer@gmail.com>
2026-09-12 22:51:29 -0300

    debugs
    no sabia si impactaba, puse debugs para encontrar el error, sacarlos antes de enviar

--- Assets/Scripts/InteractionRaycaster.cs
+++ Assets/Scripts/InteractionRaycaster.cs
@@ -9,6 +9,8 @@ public class InteractionRaycaster : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            Debug.Log("Posición del rayOrigin: " + rayOrigin.position);
+            Debug.Log("Dirección (forward): " + rayOrigin.forward);
             TryInteract();
         }
     }
@@ -18,13 +20,22 @@ public class InteractionRaycaster : MonoBehaviour
         RaycastHit hit;
         bool huboImpacto = Physics.Raycast(rayOrigin.position, rayOrigin.forward, out hit, interactionRange);
 
+        Debug.Log("Hubo impacto: " + huboImpacto);
+
         if (huboImpacto)
         {
+            Debug.Log("Le pegó a: " + hit.collider.name);
+
             IInteractable interactuable = hit.collider.GetComponent<IInteractable>();
 
             if (interactuable != null)
             {
+                Debug.Log("Tiene IInteractable, llamando Interact()");
                 interactuable.Interact();
+            }
+            else
+            {
+                Debug.Log("El objeto NO tiene IInteractable");
             }
         }
     }

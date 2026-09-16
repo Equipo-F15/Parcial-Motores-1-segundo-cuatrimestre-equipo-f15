bd35162ba9cb3b82292193e29b0e2242d79ff535
guadalupebaquer-eng <c.guadalupe.baquer@gmail.com>
2026-09-16 12:24:15 -0300

    commits de scripts
    le sgregue los commits a los scripts que habia hecho en un principio

--- Assets/Scripts/InteractionRaycaster.cs
+++ Assets/Scripts/InteractionRaycaster.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
 
-public class InteractionRaycaster : MonoBehaviour
-{
-    [SerializeField] private float interactionRange = 3f;
-    [SerializeField] private Transform rayOrigin;
-
+public class InteractionRaycaster : MonoBehaviour //se pone al jugador 
+{ 
+    [SerializeField] private float interactionRange = 3f; // es el rango que tiene el jugador para interactuar con el objeto
+    [SerializeField] private Transform rayOrigin; // donde comienza la interacción
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)) // se presiona la tecla E para interactuar y se pregunta en cada frame si se presionó
         {
             Debug.Log("Posición del rayOrigin: " + rayOrigin.position);
             Debug.Log("Dirección (forward): " + rayOrigin.forward);
@@ -15,9 +14,8 @@ public class InteractionRaycaster : MonoBehaviour
         }
     }
 
-    private void TryInteract()
-    {
-        RaycastHit hit;
+    private void TryInteract() 
+        RaycastHit hit; // se activa cuando se presiona la tecla E y se hace un raycast para ver si hay un objeto interactuable en frente del jugador, devuelve hit
         bool huboImpacto = Physics.Raycast(rayOrigin.position, rayOrigin.forward, out hit, interactionRange);
 
         Debug.Log("Hubo impacto: " + huboImpacto);
@@ -26,9 +24,9 @@ public class InteractionRaycaster : MonoBehaviour
         {
             Debug.Log("Le pegó a: " + hit.collider.name);
 
-            IInteractable interactuable = hit.collider.GetComponentInParent<IInteractable>();
+            IInteractable interactuable = hit.collider.GetComponentInParent<IInteractable>(); // buscamos si hubo impacto y se devuelve el componente IInteractable del objeto que fue impactado y sus hijos porque suele tenerlo la visagra, que seria el padre de la puerta, con la que se interactua, si no tiene devuelve null
 
-            if (interactuable != null)
+            if (interactuable != null) // si el objeto tiene IInteractable, se llama al método Interact() del objeto
             {
                 Debug.Log("Tiene IInteractable, llamando Interact()");
                 interactuable.Interact();
